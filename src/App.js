397d5d5d091d13636e64b91923b903e7f4dded2c
Aphexis <stephxca@gmail.com>
2020-02-01 01:36:51 -0500

    make geolocation fnc

--- src/App.js
+++ src/App.js
@@ -3,9 +3,45 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+
+  
+  function geoFindMe() {
+
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+  
+    mapLink.href = '';
+    mapLink.textContent = '';
+  
+    function success(position) {
+      const latitude  = position.coords.latitude;
+      const longitude = position.coords.longitude;
+  
+      status.textContent = '';
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+
+  function testFnc() {
+    alert("it worked!")
+  }
+
   return (
     <div className="App">
-      <button id="find-me" onClick="geoFindMe()">Show my location</button>
+      <button id="find-me" onClick={geoFindMe}>Show my location</button>
       <br />
       <p id="status"></p>
       <a id="map-link" target="_blank"></a>
@@ -13,35 +49,7 @@ function App() {
   );
 }
 
-function geoFindMe() {
 
-  const status = document.querySelector('#status');
-  const mapLink = document.querySelector('#map-link');
-
-  mapLink.href = '';
-  mapLink.textContent = '';
-
-  function success(position) {
-    const latitude  = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    status.textContent = '';
-    mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-    mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-  }
-
-  function error() {
-    status.textContent = 'Unable to retrieve your location';
-  }
-
-  if (!navigator.geolocation) {
-    status.textContent = 'Geolocation is not supported by your browser';
-  } else {
-    status.textContent = 'Locating…';
-    navigator.geolocation.getCurrentPosition(success, error);
-  }
-
-}
 
 // document.querySelector('#find-me').addEventListener('click', geoFindMe);
 
