15926b88147cba41150040c12305c36d7418f1b0
Rahma Gillan <rahmagillan2001@gmail.com>
2020-02-01 03:47:03 -0500

    front-end basically complete

--- src/App.js
+++ src/App.js
@@ -2,23 +2,75 @@ import React from 'react';
 import './App.css';
 import logo from './assets/bike-logo.png';
 import {Button} from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="home">
-        <h1 id="logo-title">Bike Share</h1>
-        <img id="logo" src={logo} alt={"logo"} />
-      <div className="centered-btn">
-        <Button id="my-btn" variant="success">Log In</Button>
-      </div>
-    </div>
+    <Router>
 
-    <div className="login">
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/userhome">
+            <UserHome />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-    </div>
-
-
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="default">
+        <h1 id="logo-title">Bike Share</h1>
+        <img id="logo" src={logo} alt={"logo"} />
+        <div className="centered-btn">
+        <Button id="my-btn" variant="success"><Link className="route-links" to="/login">Log In</Link></Button>
+      </div>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div className="default">
+      <h1 id="logo-title">Bike Share</h1>
+      <img id="logo" src={logo} alt={"logo"} />
+      <div className="input-g">
+        <label for="username-input" className="login-field-label">Username</label>
+        <input id="username-input" className="login-field" type="text" placeholder="Enter Username" />
+      </div>
+      <div className="input-g-outer">
+        <div className="input-g">
+          <label for="password-input" className="login-field-label">Password</label>
+          <input id="password-input" className="login-field" type="password" placeholder="Enter Password" />
+        </div>
+        <div className="centered-btn">
+          <Button id="my-btn" variant="success"><Link className="route-links" to="/userhome">Log In</Link></Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function UserHome() {
+  return (
+    <div className="default">
+      <div className="map">
+      </div>
+      <div className="centered-btn">
+        <Button id="my-btn" variant="success"><Link className="route-links" to="/">Lock</Link></Button>
+      </div>
+    </div>
+  );
+}
