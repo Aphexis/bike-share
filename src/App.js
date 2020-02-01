e50c602be50376da7878689559da89c057f185eb
Rahma Gillan <rahmagillan2001@gmail.com>
2020-02-01 01:34:13 -0500

    base html css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/bike-logo.png';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className="App">
+        <h1 id="logo-title">Bike Share</h1>
+        <img id="logo" src={logo} alt={"logo"} />
+      </div>
+    </body>
   );
 }
 
